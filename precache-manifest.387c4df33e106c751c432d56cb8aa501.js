26dd6c74f744139e3199d655a7bff4088eb5c6d3
eduardo velasquez <eduardo.velasquezv@udea.edu.co>
2020-09-18 12:45:40 -0500

    Updates

---
+++ precache-manifest.387c4df33e106c751c432d56cb8aa501.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3ca4078fcef30bde1a692b2cbcd1770",
+    "url": "/DAEVelezShops/index.html"
+  },
+  {
+    "revision": "c7d4fd16b85988d03bb3",
+    "url": "/DAEVelezShops/static/css/main.ab3e867b.chunk.css"
+  },
+  {
+    "revision": "80ae918dc3a63976692b",
+    "url": "/DAEVelezShops/static/js/2.fa5eac14.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/DAEVelezShops/static/js/2.fa5eac14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7d4fd16b85988d03bb3",
+    "url": "/DAEVelezShops/static/js/main.865a8929.chunk.js"
+  },
+  {
+    "revision": "097625dec041b100be05",
+    "url": "/DAEVelezShops/static/js/runtime-main.f31d1f54.js"
+  }
+]);
